a7741a16aa020936123de58ad701142be7895711
travism26 <travismartin266@gmail.com>
2020-04-04 20:14:24 -0300

    Finished for now

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = {
     videos: [],
     selectedVideo: null
   };
+
+  componentDidMount() {
+    this.onTermSubmit('Never Gonna Give You Up');
+  }
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
@@ -15,22 +20,34 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
     // our videos are stored at: response.data.items
   };
 
   onVideoSelect = video => {
-    console.log("From the app:", video);
+    this.setState({ selectedVideo: video });
   };
-  
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,10 +2,11 @@ import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video, onVideoSelect }) => {
-  console.log(video);
+//   console.log(video);
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
+        alt={video.snippet.title}
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
         id={video.id}
